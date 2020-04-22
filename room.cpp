a019a963b8325698a69871fc7c25cfba36d83ced
anayep <bizzare.stuffz@yahoo.com>
2020-04-22 23:48:25 +0300

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -28,27 +28,4 @@ int room::getroomnr ()
     return roomnr;
 }
 
-/* int room::setdate(int n){
 
- int ok2,ok,i,k,j;
- ok=1;
- ok2=1;
- k=0;
- while(ok2==1){
-    ok=1;
-
-    while(date[k]!=0)
-        k++;
-
-    ok=0;
-    for(i=k;i<k+n;i++)
-        if(date[i]==1) { ok=1; break;}
-
-    if(ok==1) k=k+n;
-    if(ok==0){
-        for(j=k;j<k+n;j++)
-            date[j]=1;
-        ok2=0;
-    }
- }}
-*/
